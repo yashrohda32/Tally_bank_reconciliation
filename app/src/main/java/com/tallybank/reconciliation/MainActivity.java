9a4943b0a51cc0381e9ce08be194a09ba7b5354a
yashrohda32 <yashrohda32@gmail.com>
2026-09-11 17:27:55 +0530

    Set up WebView in MainActivity
    Initialize WebView with settings and load local HTML file.

--- app/src/main/java/com/tallybank/reconciliation/MainActivity.java
+++ app/src/main/java/com/tallybank/reconciliation/MainActivity.java
@@ -1,1 +1,33 @@
+package com.tallybank.reconciliation;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        WebView webView = new WebView(this);
+
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setAllowFileAccess(true);
+        settings.setAllowContentAccess(true);
+        settings.setJavaScriptCanOpenWindowsAutomatically(true);
+        settings.setSupportZoom(false);
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.setWebChromeClient(new WebChromeClient());
+
+        webView.loadUrl("file:///android_asset/index.html");
+
+        setContentView(webView);
+    }
+}
